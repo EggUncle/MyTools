a7da9cb5d1464e411d7d559d5a041b4b77e0fa75
EggUncle <cicadashadow@gmail.com>
2016-08-17 21:00:08 +0800

    添加了百度的APIstore

--- app/src/main/java/uncle/egg/mytools/activity/MyApplication.java
+++ app/src/main/java/uncle/egg/mytools/activity/MyApplication.java
@@ -4,22 +4,27 @@ import android.app.Application;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.baidu.apistore.sdk.ApiStoreSDK;
 
 /**
  * Created by egguncle on 16.7.19.
  */
 public class MyApplication extends Application {
 
-    public static RequestQueue queue;
+  //  public static RequestQueue queue;
+
+    private String MY_API_KEY="994f4d03499d618879bd337a94bce437";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        queue = Volley.newRequestQueue(getApplicationContext());
+   //     queue = Volley.newRequestQueue(getApplicationContext());
+        ApiStoreSDK.init(this,MY_API_KEY);
+
     }
 
-    public static RequestQueue getHttpQueues(){
-        return queue;
-    }
+  //  public static RequestQueue getHttpQueues(){
+   //     return queue;
+  //  }
 
 }
